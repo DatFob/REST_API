64aea6b53d56ce4b3db0a7ea6a51a95177d4bf39
Mike Z <tzhao1@scu.edu>
2021-12-14 16:46:51 -0800

    All features finished

---
+++ userInfo/UserInfoConsstants.java
@@ -0,0 +1,9 @@
+package userInfo;
+
+public class UserInfoConsstants {
+    public static final String NAME_FORM = "<form name=\"userForm\" method=\"post\" action=\"modifyUser\">\n" +
+            "    New Firstname: <input type=\"text\" name=\"firstName\"/> <br/>\n" +
+            "    New Lastname: <input type=\"text\" name=\"lastName\"/> <br/>\n" +
+            "    <input type=\"submit\" value=\"Submit\" />\n" +
+            "</form>";
+}
---
+++ utilities/TwitterConfig.java
@@ -0,0 +1,31 @@
+package utilities;
+
+public class TwitterConfig {
+    private String access_token;
+    private String access_token_secret;
+    private String consumer_key;
+    private String consumer_key_secret;
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public String getAccess_token_secret() {
+        return access_token_secret;
+    }
+
+    public String getConsumer_key() {
+        return consumer_key;
+    }
+
+    public String getConsumer_key_secret() {
+        return consumer_key_secret;
+    }
+
+    public TwitterConfig(String access_token, String access_token_secret, String consumer_key, String consumer_key_secret) {
+        this.access_token = access_token;
+        this.access_token_secret = access_token_secret;
+        this.consumer_key = consumer_key;
+        this.consumer_key_secret = consumer_key_secret;
+    }
+}
